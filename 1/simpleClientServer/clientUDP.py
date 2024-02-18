08e41f92c1df69f537668e19eec2d764de8055e6
Dorman <dorman8288@gmail.com>
2024-02-18 21:03:01 +0330

    added Client

---
+++ 1/simpleClientServer/clientUDP.py
@@ -0,0 +1,13 @@
+from socket import *
+
+serverIP = ''
+serverPort = 100
+clientSocket = socket(AF_INET, SOCK_DGRAM)
+try:
+    while True:
+        message = input("Please enter Your Query:\n")
+        clientSocket.sendto(message.encode(), (serverIP, serverPort))
+        result, serverAddress = clientSocket.recv(2048)
+        print(f"your proccessed query is: {result.decode()}")
+finally:
+    clientSocket.close()    
