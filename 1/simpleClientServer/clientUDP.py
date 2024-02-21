aea7b1f29f8d6f46d7266dd3ed2bc9f91c899e0d
Dorman8288 <dorman8288@gmail.com>
2024-02-21 14:38:07 +0330

    client

--- 1/simpleClientServer/clientUDP.py
+++ 1/simpleClientServer/clientUDP.py
@@ -1,13 +1,11 @@
 from socket import *
-
-serverIP = ''
+import time
+serverIP = '192.168.1.113'
 serverPort = 100
 clientSocket = socket(AF_INET, SOCK_DGRAM)
-try:
-    while True:
-        message = input("Please enter Your Query:\n")
-        clientSocket.sendto(message.encode(), (serverIP, serverPort))
-        result, serverAddress = clientSocket.recv(2048)
-        print(f"your proccessed query is: {result.decode()}")
-finally:
-    clientSocket.close()    
+while True:
+    message = input("Please enter Your Query:\n")
+    clientSocket.sendto(message.encode(), (serverIP, serverPort))
+    result, serverAddress = clientSocket.recvfrom(2048)
+    print(f"your proccessed query is: {result.decode()}")
+clientSocket.close()    
